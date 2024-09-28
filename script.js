2a1a446891f392cd3a77e340f3f2b8f653d72f1a
blueorks <114741623+blueorks@users.noreply.github.com>
2024-09-28 22:42:49 +0500

    Implement playRound()
    Implements the functionality of a single game
    round

--- script.js
+++ script.js
@@ -16,3 +16,41 @@ function getHumanChoice() {
   let humanChoice = prompt("Select your weapon: rock paper scissors");
   return humanChoice.toLowerCase();   // incase the user enters a capitalized string
 }
+
+function playRound(humanChoice, computerChoice) {
+  
+  if(humanChoice.localeCompare(computerChoice) == 0)
+    console.log("Tie! Both chose " + humanChoice);
+  else if(humanChoice.localeCompare("rock") == 0) {
+    if(computerChoice.localeCompare("paper") == 0) {
+      console.log("You Lost! Paper beats Rock");
+      ++computerScore;
+    }
+    else {
+      console.log("You Won! Rock beats Scissors");
+      ++humanScore;
+    }
+  }
+  else if(humanChoice.localeCompare("paper") == 0) {
+    if(computerChoice.localeCompare("rock") == 0) {
+      console.log("You Won! Paper beats Rock");
+      ++humanScore;
+    }
+    else {
+      console.log("You Lost! Scissors beats Paper");
+      ++computerScore;
+    }
+  }
+  else {
+    if(computerChoice.localeCompare("rock") == 0) {
+      console.log("You Lost! Rock beats Scissors");
+      ++computerScore;
+    }
+    else {
+      console.log("You Won! Scissors beats Paper");
+      ++humanScore;
+    }
+  }
+}
+
+let humanScore = 0, computerScore = 0;
