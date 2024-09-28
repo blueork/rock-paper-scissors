2b5ee85d6d6d16d6b62b4032059b157a92522a66
blueorks <114741623+blueorks@users.noreply.github.com>
2024-09-28 22:53:02 +0500

    Implement playGame()
    Implement playGame() which runs the entire game.
    Move humanScore, computerScore, playRound() within
    the playGame() function.

--- script.js
+++ script.js
@@ -17,40 +17,61 @@ function getHumanChoice() {
   return humanChoice.toLowerCase();   // incase the user enters a capitalized string
 }
 
-function playRound(humanChoice, computerChoice) {
+//let humanScore = 0, computerScore = 0;
+
+function playGame() {
+  let humanScore = 0, computerScore = 0;
+
+  function playRound(humanChoice, computerChoice) {
   
-  if(humanChoice.localeCompare(computerChoice) == 0)
-    console.log("Tie! Both chose " + humanChoice);
-  else if(humanChoice.localeCompare("rock") == 0) {
-    if(computerChoice.localeCompare("paper") == 0) {
-      console.log("You Lost! Paper beats Rock");
-      ++computerScore;
+    if(humanChoice.localeCompare(computerChoice) == 0)
+      console.log("Tie! Both chose " + humanChoice);
+    else if(humanChoice.localeCompare("rock") == 0) {
+      if(computerChoice.localeCompare("paper") == 0) {
+        console.log("You Lost! Paper beats Rock");
+        ++computerScore;
+      }
+      else {
+        console.log("You Won! Rock beats Scissors");
+        ++humanScore;
+      }
+    }
+    else if(humanChoice.localeCompare("paper") == 0) {
+      if(computerChoice.localeCompare("rock") == 0) {
+        console.log("You Won! Paper beats Rock");
+        ++humanScore;
+      }
+      else {
+        console.log("You Lost! Scissors beats Paper");
+        ++computerScore;
+      }
     }
     else {
-      console.log("You Won! Rock beats Scissors");
-      ++humanScore;
+      if(computerChoice.localeCompare("rock") == 0) {
+        console.log("You Lost! Rock beats Scissors");
+        ++computerScore;
+      }
+      else {
+        console.log("You Won! Scissors beats Paper");
+        ++humanScore;
+      }
     }
   }
-  else if(humanChoice.localeCompare("paper") == 0) {
-    if(computerChoice.localeCompare("rock") == 0) {
-      console.log("You Won! Paper beats Rock");
-      ++humanScore;
-    }
-    else {
-      console.log("You Lost! Scissors beats Paper");
-      ++computerScore;
-    }
+
+  let numOfRounds = 5;
+
+  for(let i = 0; i < numOfRounds; ++i) {
+
+    playRound(getHumanChoice(), getComputerChoice());
   }
-  else {
-    if(computerChoice.localeCompare("rock") == 0) {
-      console.log("You Lost! Rock beats Scissors");
-      ++computerScore;
-    }
-    else {
-      console.log("You Won! Scissors beats Paper");
-      ++humanScore;
-    }
-  }
+
+  if(humanScore == computerScore)
+    console.log("Tie! Both scored " + humanScore);
+  else if(humanScore > computerScore)
+    console.log("You Won! " + humanScore + " vs " + computerScore);
+  else
+    console.log("You Lost! " + humanScore + " vs " + computerScore);
+
 }
 
-let humanScore = 0, computerScore = 0;
+playGame();
