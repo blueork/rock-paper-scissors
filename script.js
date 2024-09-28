fa3be4f96f358311f8941cfbe5f6661725303fde
blueorks <114741623+blueorks@users.noreply.github.com>
2024-09-28 22:23:36 +0500

    Implement getComputerChoice()
    Uses Math.random() to generate computer choice

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("Hello World");
+
+
+function getComputerChoice() {
+  let choice = Math.floor(Math.random() * 100);
+
+  if(choice % 3 == 0)
+    return "rock";
+  else if(choice % 5 == 0)
+    return "paper";
+  else
+    return "scissors";
+}
